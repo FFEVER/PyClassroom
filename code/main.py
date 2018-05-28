eab5e448019007287bac6b41ffb1b646cb5a9d56
Panupong <most54321@gmail.com>
2018-05-29 05:29:15 +0700

    Minor fix

--- code/main.py
+++ code/main.py
@@ -40,7 +40,7 @@ class SelectRole(QtWidgets.QMainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv) 
-    w = StudentLogic() 
+    w = SelectRole() 
     w.show()
     sys.exit(app.exec_())
     print("Exit")
